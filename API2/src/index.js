53b4314e487ba714f2063160cc52b9dbf565703d
MartinLova <martin.lovagnini@gmail.com>
2023-10-22 11:19:55 -0300

    antes de insomnia

--- API2/src/index.js
+++ API2/src/index.js
@@ -2,20 +2,23 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 
+//accedemos a la funcionalidad de express
 const server = express()
 
-//nuestra api va a proporcionar los datos en dicha anotación
+//nuestra api va a proporcionar los datos en dicha anotación_otro comentario: aca dejo configurado la notación que manejara el flujo de datos
 server.use(express.json())
 
+//permitirá comunicación entre cliente y servidor
 server.use(cors())
 
-server.use(morgan('dev'))
+server.use(morgan('dev')) //morgan es una dependencia de desarrollo que la vamos a usar durante el proceso de construcción de la API, nos informará por consola todas las solicitudes http que vayan surgiendo
 
+//pongo en funcionamiento las rutas existentes dentro de cliente.route
 server.use(require('./routes/cliente.route'))
 
-server.set('port',process.env.PORT || 3001)
+server.set('port',process.env.PORT || 3001)//puesta en marcha indicar el puerto por el cual va a correr el servidor que en este caso es el 3001 o el indicado en la variable de entorno PORT que suele estar disponible en servidores
 
-//ponemos en marcha el servidor
+//iniciando el servidor
 server.listen(server.get('port'))
 
 console.log('Servidor corriendo en puerto: ',server.get('port'))
--- API2/src/routes/cliente.route.js
+++ API2/src/routes/cliente.route.js
@@ -16,4 +16,14 @@ router.get('/clientes', async(req,res) => {
     res.json(rows)
 })
 
+router.post('/cliente', async(req,res) => {
+
+    const unCliente = req.body;
+
+    const db = await conexion.obtener_conexion()
+
+    await db.query(`insert into cliente (nombre,apellido,dni)
+    VALUES ('${unCliente.nombre}','${unCliente.apellido}',${unCliente.dni})`)
+    res.json('El cliente se guardó exitosamente!')
+})
 module.exports = router
